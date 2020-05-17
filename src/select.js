187e93cbddd013e35840566ae98c5afa6fccbeab
xiaosatufu <xiaosatufu@163.com>
2020-05-17 13:15:10 +0800

    f

--- src/select.js
+++ src/select.js
@@ -42,14 +42,58 @@ const { Blog, User } = require('./model')
 
 
     //查询总数
-    const blogListAndCount = await Blog.findAndCountAll({
-        limit: 2, //限制每次查询2条
-        offset: 2,  //跳过多少条
-        order: [['id', 'desc']]
+    // const blogListAndCount = await Blog.findAndCountAll({
+    //     limit: 2, //限制每次查询2条
+    //     offset: 2,  //跳过多少条
+    //     order: [['id', 'desc']]
 
+    // })
+
+    // console.log('blogListAndCount', blogListAndCount.count, blogListAndCount.rows.map(blog => blog.dataValues))
+
+    //连表查询1
+    // const blogListWithUser = await Blog.findAndCountAll({
+    //     order: [
+    //         ['id', 'desc']
+    //     ],
+    //     include: [
+    //         {
+    //             model: User,
+    //             attributes: ['userName', 'nickName'],
+    //             where: {
+    //                 userName: 'zhangsan'
+    //             }
+    //         }
+    //     ]
+    // })
+
+    // console.log('blogListWithUser', blogListWithUser.count, blogListWithUser.rows.map(blog => {
+    //     const blogVal = blog.dataValues
+    //     blogVal.user = blogVal.user.dataValues
+    //     return blogVal
+    // }))
+
+
+    //连表查询2
+
+    const userListWithBlog = await User.findAndCountAll({
+        attributes: ['userName', 'nickName'],
+        include:[
+            {
+                model:Blog
+            }
+        ]
     })
 
-    console.log('blogListAndCount', blogListAndCount.count, blogListAndCount.rows.map(blog => blog.dataValues))
+
+    console.log('userListWithBlog',userListWithBlog.count ,userListWithBlog.rows.map(user=>{
+        const userVal = user.dataValues
+        userVal.blogs = userVal.blogs.map(blog=>blog.dataValues)
+        return userVal
+    }))
+
+
+
 
 
 })()
--- src/seq.js
+++ src/seq.js
@@ -4,6 +4,11 @@ const conf = {
     host: 'localhost',
     dialect: 'mysql'
 }
+conf.pool = {
+    max: 5,
+    min: 0,
+    idle: 10000 //如果一个连接池10s之内没有被使用，则释放
+}
 const seq = new Sequelize('koa-weibo-db', 'root', 'xiaosatufu', conf)
 
 //测试链接
